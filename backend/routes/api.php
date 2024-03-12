84ca8d475fc4d713c776aba41cb99c95886684e5
Pol <polsamo2003@gmail.com>
2024-03-12 20:49:22 +0100

    Rutas de apis creada

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::middleware('auth:sanctum')->get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::middleware('auth:sanctum')->get('/products', [App\Http\Controllers\Product::class, 'index'])->name('products');
+// Route::middleware('auth:sanctum')->post('/login', [App\Http\Controllers\Auth\LoginController::class])->name('login');
+
+Route::post("register", [ApiController::class, "register"])->name("register");
+Route::post("login", [ApiController::class, "login"])->name("login");
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get("profile", [ApiController::class, "profile"])->name("profile");
+    Route::get("logout", [ApiController::class, "logout"])->name("logout");
+    Route::get("products", [ProductController::class, "index"])->name("products");
 });
