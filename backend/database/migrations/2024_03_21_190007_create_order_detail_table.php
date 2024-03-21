8c89f0800200cc5da3fdccb3c8f8e9725799af5f
Pol <polsamo2003@gmail.com>
2024-03-21 16:36:17 +0100

    Migraciones actualizadas

---
+++ backend/database/migrations/2024_03_21_190007_create_order_detail_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('variant_id')->constrained('product_variants')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('purchase_price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_detail');
+    }
+};
