e1e65404960845c53d2433543188bd3d15c5070b
Pol <polsamo2003@gmail.com>
2024-03-30 14:59:51 +0100

    OrderController creado

---
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $orders = Order::all();
+        return OrderResource::collection($orders);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $order = new Order;
+
+        $order->user_id = Auth::user()->id;
+        $order->order_date = Carbon::now();
+
+        $order->save();
+
+        return response()->json([
+            "message" => "Order stored successfully",
+            'order' => $order
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message' => 'Order not found'], 404);
+        }
+        return new OrderResource($order);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $order = Order::find($id);
+
+        if (!$order) {
+            return response()->json(['message' => 'Order not found'], 404);
+        }
+
+        $order->delete();
+
+        return response()->json(['message' => 'Order deleted successfully'], 200);
+    }
+}
