1cabe4399ac476794ab4329abed5c6c2a2f91e07
Pol <polsamo2003@gmail.com>
2024-04-02 16:34:40 +0200

    Validador añadido

--- backend/app/Helpers/ManageImage.php
+++ backend/app/Helpers/ManageImage.php
@@ -1,21 +1,29 @@
 <?php
 
 namespace App\Helpers;
+
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 
-class ManageImage {
+class ManageImage
+{
 
-    public static function storeImage($imagePath) {
+    public static function storeImage($imagePath)
+    {
         $path = $imagePath->store('images');
         $imageName = preg_replace("~^.*/~", "", $path);
 
         return $imageName;
     }
 
-    public static function getImage($imageName) {
+    public static function getImage($imageName)
+    {
         $image = Storage::disk('images')->get($imageName);
-        
+        if (!$image) {
+            return response()->json(['message' => 'Image not found'], 404);
+        }
         return new Response($image, 200);
     }
 
