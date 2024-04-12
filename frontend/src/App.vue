d90e5d03926ff80f99ad970d32b96672ab579198
Pol <polsamo2003@gmail.com>
2024-04-12 19:23:11 +0200

    Frontend Vue creado

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import MainNavbar from './components/MainNavbar.vue';
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <MainNavbar />
+        </div>
+    </header>
+
+    <RouterView />
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import './assets/main.css';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
