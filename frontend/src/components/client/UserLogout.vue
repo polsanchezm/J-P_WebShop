a169319c77c31e0ae95a4bba62aaf7d28cb168d5
Pol <polsamo2003@gmail.com>
2024-04-12 19:23:49 +0200

    Componente creado

---
+++ frontend/src/components/client/UserLogout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/client/user';
+
+const userStore = useUserStore();
+userStore.userLogout();
+</script>
+
+<template>
+    <div></div>
+</template>
