d0d18a287f5d0a3ad3a3dce10c8e35a983f7cee1
Pol <polsamo2003@gmail.com>
2024-04-12 19:24:40 +0200

    Vistas creadas

---
+++ frontend/src/views/client/UserLoginView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UserLogin from '@/components/client/UserLogin.vue';
+</script>
+
+<template>
+    <main>
+        <UserLogin />
+    </main>
+</template>
---
+++ frontend/src/views/client/UserRegisterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UserRegister from '@/components/client/UserRegister.vue';
+</script>
+
+<template>
+    <main>
+        <UserRegister />
+    </main>
+</template>
