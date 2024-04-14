a22b5790bc70fb6f76ae006785450d98e7648b60
Pol <polsamo2003@gmail.com>
2024-04-14 13:08:39 +0200

    Rutas añadidas

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,6 +4,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            name: 'home',
+            component: () => import('@/components/MainHome.vue')
+        },
+        {
             path: '/login',
             name: 'login',
             component: () => import('@/views/client/UserLoginView.vue')
@@ -19,22 +24,21 @@ const router = createRouter({
             component: () => import('@/components/client/UserLogout.vue')
         },
         {
-            path: '/home',
-            name: 'home',
-            component: () => import('@/components/MainHome.vue')
-        },
-
-        // Detail & Edit added
-        {
-            path: '/detail', // Modificar después
-            name: 'detail',
-            component: () => import('@/views/client/profile/UserDetailView.vue')
-        },
-        {
-            path: '/edit', // Modificar después
-            name: 'edit',
-            component: () => import('@/views/client/profile/UserEditView.vue')
-        },
+            path: '/user',
+            name: 'user',
+            children: [
+                {
+                    path: '/user/detail', // Modificar después
+                    name: 'detail',
+                    component: () => import('@/views/client/profile/UserDetailView.vue')
+                },
+                {
+                    path: '/user/edit', // Modificar después
+                    name: 'edit',
+                    component: () => import('@/views/client/profile/UserEditView.vue')
+                }
+            ]
+        }
     ]
 });
 
