ac840369b23965a1e791e5549c4b67215473257e
Pol <polsamo2003@gmail.com>
2024-04-16 16:21:57 +0200

    Comprobar el token expiry al cargar la web

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import MainNavbar from './components/MainNavbar.vue';
+import { onMounted } from 'vue';
+import { useUserStore } from '@/stores/client/user';
+const userStore = useUserStore();
+onMounted(() => {
+    userStore.checkTokenExpiry();
+})
+
 </script>
 
 <template>
