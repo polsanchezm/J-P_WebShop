8acd10ef9505dab54ed979df23f4612a89346a91
Pol <polsamo2003@gmail.com>
2024-04-16 16:23:15 +0200

    Logica mejorada

--- frontend/src/components/client/profile/UserDetail.vue
+++ frontend/src/components/client/profile/UserDetail.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/client/user';
-import { computed } from 'vue';
-
+import { onBeforeMount } from 'vue';
 const userStore = useUserStore();
-userStore.userDetail();
-const user = computed(() => userStore.user);
-const loggedUser = userStore.userDetail().then(loggedUser => {
-    user.value.id = loggedUser.id;
-    user.value.name = loggedUser.name;
-    user.value.surnames = loggedUser.surnames;
-    user.value.birthdate = new Date(loggedUser.birthdate);
-    user.value.email = loggedUser.email;
-}
-);
 
-const formatDate = (date: Date): string => {
+onBeforeMount(async () => {
+    await userStore.userDetail();
+    console.log('user', userStore.user);
+});
+
+const formatDate = (date: string | Date | null): string => {
+    if (date === null) {
+        return 'Date not provided';
+    }
+    if (typeof date === 'string') {
+        date = new Date(date);
+    }
     return new Intl.DateTimeFormat(navigator.language, { month: 'long', day: 'numeric', year: 'numeric' }).format(date);
 };
 </script>
 
 <template>
-    <div class="max-w-md mx-auto bg-white shadow-md rounded px-4 py-6">
+    <div v-if="userStore.user" class="max-w-md mx-auto bg-white shadow-md rounded px-4 py-6">
         <p class="text-lg font-semibold mb-2 text-gray-700">User Information</p>
         <div>
-            <p class="text-gray-700"><span class="font-semibold">ID:</span> {{ user.id }}</p>
-            <p class="text-gray-700"><span class="font-semibold">Name:</span> {{ user.name }}</p>
-            <p class="text-gray-700"><span class="font-semibold">Surnames:</span> {{ user.surnames }}</p>
-            <p class="text-gray-700"><span class="font-semibold">Birthdate:</span> {{ user.birthdate ?
-                formatDate(user.birthdate) : 'N/A' }}</p>
-            <p class="text-gray-700"><span class="font-semibold">Email:</span> {{ user.email }}</p>
+            <p class="text-gray-700"><span class="font-semibold">ID:</span> {{ userStore.user!.id }}</p>
+            <p class="text-gray-700"><span class="font-semibold">Name:</span> {{ userStore.user!.name }}</p>
+            <p class="text-gray-700"><span class="font-semibold">Surnames:</span> {{ userStore.user!.surnames }}</p>
+            <p class="text-gray-700"><span class="font-semibold">Birthdate:</span> {{
+                formatDate(userStore.user!.birthdate) }}</p>
+            <p class="text-gray-700"><span class="font-semibold">Email:</span> {{ userStore.user!.email }}</p>
             <RouterLink
                 class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none"
                 to="/user/edit">Edit</RouterLink>
--- frontend/src/components/client/profile/UserEdit.vue
+++ frontend/src/components/client/profile/UserEdit.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/client/user';
-import { computed } from 'vue';
-
-const user = computed(() => userStore.user);
+import { onBeforeMount } from 'vue';
 const userStore = useUserStore();
 
-const loggedUser = userStore.userDetail().then(loggedUser => {
-    user.value.id = loggedUser.id;
-    user.value.name = loggedUser.name;
-    user.value.surnames = loggedUser.surnames;
-    user.value.birthdate = loggedUser.birthdate;
-    user.value.email = loggedUser.email;
-}
-);
+onBeforeMount(async () => {
+    await userStore.userDetail();
+    if (typeof userStore.user!.birthdate === 'string') {
+        userStore.user!.birthdate = new Date(userStore.user!.birthdate);
+        userStore.user!.birthdate = userStore.user!.birthdate.toISOString().slice(0, 10);
+    }
+});
 </script>
 
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section v-if="userStore.user" class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div
                 class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -24,61 +21,57 @@ const loggedUser = userStore.userDetail().then(loggedUser => {
                     <h1
                         class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Edit details</h1>
-                    <form class="space-y-4 md:space-y-6" @submit.prevent="userStore.userEdit(user)">
+                    <form class="space-y-4 md:space-y-6" @submit.prevent="userStore.userEdit(userStore.user)">
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                 name</label>
-                            <input type="text" v-model="user.name" name="name" id="name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="John" required />
+                            <input type="text" v-model="userStore.user!.name" name="name" id="name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div>
                             <label for="surnames"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                 surnames</label>
-                            <input type="text" v-model="user.surnames" name="surnames" id="surnames"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Doe" required />
+                            <input type="text" v-model="userStore.user!.surnames" name="surnames" id="surnames"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                 email</label>
-                            <input type="email" v-model="user.email" name="email" id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="johnDoe@mail.com" required />
+                            <input type="email" v-model="userStore.user!.email" name="email" id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                birthdate</label>
-                            <input type="date" v-model="user.birthdate" name="birthdate" id="birthdate"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="00-00-0000" required />
+                        <div class="flex flex-wrap -mx-3">
+                            <div class="w-full px-3">
+                                <!-- Añadido mb-6 para más espacio debajo del campo de contraseña actual -->
+                                <label for="password"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current
+                                    Password</label>
+                                <input type="password" v-model="userStore.user!.currentPassword" name="password"
+                                    id="password" placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            <div class="w-1/2 px-3 mt-6"> <!-- Añadido mt-6 para más espacio arriba -->
+                                <label for="new-password"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New
+                                    Password</label>
+                                <input type="password" v-model="userStore.user!.newPassword" name="new-password"
+                                    id="new-password" placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            <div class="w-1/2 px-3 mt-6"> <!-- Añadido mt-6 para mantener la consistencia visual -->
+                                <label for="confirm-new-password"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm New
+                                    Password</label>
+                                <input type="password" v-model="userStore.user!.newPasswordConfirmation"
+                                    name="confirm-new-password" id="confirm-new-password" placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
                         </div>
-                        <div>
-                            <label for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" v-model="user.password" name="password" id="password"
-                                placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
-                        </div>
-                        <div>
-                            <label for="confirm-password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
-                                password</label>
-                            <input type="password" v-model="user.password_confirmation" name="confirm-password"
-                                id="confirm-password" placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
-                        </div>
+
                         <button type="submit"
                             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Edit
                             details</button>
-                        <!-- <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account?
-
-                            <RouterLink to="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here </RouterLink>
-                        </p> -->
                     </form>
                 </div>
             </div>
