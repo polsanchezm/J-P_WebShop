55ceaa5178dfd14c4c91263319f8aad345261922
jeff-mcsc <129522162+jeff-mcsc@users.noreply.github.com>
2024-04-18 13:18:02 +0200

    Orders i OrdersDetails Views fetes

---
+++ frontend/src/views/client/profile/UserOrderDetailView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import UserOrderDetail from '@/components/client/profile/UserOrderDetail.vue';
+</script>
+
+<template>
+    <div>
+        <UserOrderDetail />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/client/profile/UserOrdersView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import UserOrders from '@/components/client/profile/UserOrders.vue';
+</script>
+
+<template>
+    <div>
+        <UserOrders />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
