5af88e8c9f44478e862b24eaa421d0059947657d
Pol <polsamo2003@gmail.com>
2024-04-18 11:22:51 +0200

    Mejoras hechas

--- frontend/src/components/client/UserRegister.vue
+++ frontend/src/components/client/UserRegister.vue
@@ -16,7 +16,7 @@ const user = ref<User>({
     currentPassword: '',
     currentPasswordConfirmation: '',
     newPassword: '',
-    newPasswordConfirmation: '',
+    newPasswordConfirmation: ''
 });
 
 // const schema = yup.object({
@@ -34,7 +34,6 @@ const user = ref<User>({
 //     validationSchema: schema
 // });
 
-
 // const [name, nameAttrs] = defineField('name');
 // const [surnames, surnamesAttrs] = defineField('surnames');
 // const [email, emailAttrs] = defineField('email');
@@ -49,90 +48,56 @@ const user = ref<User>({
 // });
 </script>
 
-
 <template>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1
-                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Create an account</h1>
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create an account</h1>
                     <form @submit.prevent="authStore.userRegister(user)" class="space-y-4 md:space-y-6">
                         <!-- <form @submit.prevent="onSubmit" class="space-y-4 md:space-y-6"> -->
                         <div>
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                name</label>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
                             <!-- <input type="text" v-model="name" v-bind="nameAttrs" name="name" id="name" -->
-                            <input type="text" v-model="user.name" name="name" id="name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="John" required />
+                            <input type="text" v-model="user.name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
                             <!-- <span v-if="errors.name" class="text-red-500">{{ errors.name }}</span> -->
                         </div>
                         <div>
-                            <label for="surnames"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                surnames</label>
+                            <label for="surnames" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your surnames</label>
                             <!-- <input type="text" v-model="surnames" v-bind="surnamesAttrs" name="surnames" id="surnames" -->
-                            <input type="text" v-model="user.surnames" name="surnames" id="surnames"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Doe" required />
+                            <input type="text" v-model="user.surnames" name="surnames" id="surnames" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
                             <!-- <span v-if="errors.surnames" class="text-red-500">{{ errors.surnames }}</span> -->
-
                         </div>
                         <div>
-                            <label for="birthdate"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                birthdate</label>
+                            <label for="birthdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your birthdate</label>
                             <!-- <input type="text" v-model="surnames" v-bind="surnamesAttrs" name="surnames" id="surnames" -->
-                            <input type="date" v-model="user.birthdate" name="birthdate" id="birthdate"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
+                            <input type="date" v-model="user.birthdate" name="birthdate" id="birthdate" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             <!-- <span v-if="errors.surnames" class="text-red-500">{{ errors.surnames }}</span> -->
-
                         </div>
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                email</label>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <!-- <input type="email" v-model="email" v-bind="emailAttrs" name="email" id="email" -->
-                            <input type="email" v-model="user.email" name="email" id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="johnDoe@mail.com" required />
+                            <input type="email" v-model="user.email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="johnDoe@mail.com" required />
                             <!-- <span v-if="errors.email" class="text-red-500">{{ errors.email }}</span> -->
-
                         </div>
                         <div>
-                            <label for="password"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <!-- <input type="password" v-model="password" v-bind="passwordAttrs" name="password" -->
-                            <input type="password" v-model="user.currentPassword" name="password" id="password"
-                                placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
+                            <input type="password" v-model="user.currentPassword" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             <!-- <span v-if="errors.password" class="text-red-500">{{ errors.password }}</span> -->
                         </div>
                         <div>
-                            <label for="passwordConfirmation"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
-                                password</label>
+                            <label for="passwordConfirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                             <!-- <input type="password" v-model="passwordConfirmation" v-bind="passwordConfirmationAttrs" -->
-                            <input type="password" v-model="user.currentPasswordConfirmation"
-                                name="passwordConfirmation" id="password_confirmation" placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
+                            <input type="password" v-model="user.currentPasswordConfirmation" name="passwordConfirmation" id="password_confirmation" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             <!-- <span v-if="errors.passwordConfirmation" class="text-red-500">{{ errors.passwordConfirmation
                                 }}</span> -->
                         </div>
-                        <button type="submit"
-                            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
-                            an account</button>
+                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Already have an account?
 
-                            <RouterLink to="/login"
-                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here
-                            </RouterLink>
+                            <RouterLink :to="{ name: 'login' }" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here </RouterLink>
                         </p>
                     </form>
                 </div>
