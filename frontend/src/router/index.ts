9c20859a68de6e01a0798abff1d1304d2f449da7
Pol <polsamo2003@gmail.com>
2024-04-18 11:23:53 +0200

    Rutas añadidas

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,3 +1,5 @@
+import { isAuthenticatedGuard } from '@/guards/authGuard';
+import { setupTokenExpiryGuard } from '@/guards/expiryGuard';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
@@ -26,20 +28,63 @@ const router = createRouter({
         {
             path: '/user',
             name: 'user',
+            // redirect: '/user/detail',
             children: [
                 {
-                    path: '/user/detail', // Modificar después
-                    name: 'detail',
-                    component: () => import('@/views/client/profile/UserDetailView.vue')
+                    path: '',
+                    name: 'user.detail',
+                    component: () => import('@/views/client/profile/UserDetailView.vue'),
+                    meta: { requiresAuth: true }
                 },
                 {
-                    path: '/user/edit', // Modificar después
-                    name: 'edit',
-                    component: () => import('@/views/client/profile/UserEditView.vue')
+                    path: 'edit',
+                    name: 'user.edit',
+                    component: () => import('@/views/client/profile/UserEditView.vue'),
+                    meta: { requiresAuth: true }
+                },
+                {
+                    path: 'shipping',
+                    name: 'shipping',
+                    children: [
+                        {
+                            path: '',
+                            name: 'shipping.all',
+                            component: () => import('@/views/client/profile/shipping/AllShippingsView.vue'),
+                            meta: { requiresAuth: true }
+                        },
+                        {
+                            path: 'create',
+                            name: 'shipping.create',
+                            component: () => import('@/views/client/profile/shipping/ShippingCreateView.vue'),
+                            meta: { requiresAuth: true }
+                        },
+                        {
+                            path: 'edit/:id',
+                            name: 'shipping.edit',
+                            component: () => import('@/views/client/profile/shipping/ShippingEditView.vue'),
+                            meta: { requiresAuth: true }
+                        },
+                        {
+                            path: 'delete/:id',
+                            name: 'shipping.delete',
+                            component: () => import('@/components/client/profile/shipping/ShippingDelete.vue'),
+                            meta: { requiresAuth: true }
+                        }
+                    ]
                 }
             ]
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'error404',
+            component: () => import('@/views/Error404View.vue')
         }
     ]
+});
+
+router.beforeEach(async (to, from, next) => {
+    await isAuthenticatedGuard(to, from, next);
+    await setupTokenExpiryGuard(to, from, next);
 });
 
 export default router;
