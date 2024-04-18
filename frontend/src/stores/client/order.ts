f748128995753ee1d5bdc9c5ed563781501a5d96
jeff-mcsc <129522162+jeff-mcsc@users.noreply.github.com>
2024-04-18 13:17:12 +0200

    Order Store fet

---
+++ frontend/src/stores/client/order.ts
@@ -0,0 +1,104 @@
+import { defineStore } from "pinia";
+import axios, { type ErrorResponse, type ApiResponse } from '@/lib/axios';
+import router from '@/router';
+import { ref } from 'vue';
+import { type Order } from "@/models/order";
+import { type OrderDetail } from '@/models/orderDetail';
+
+export const useOrderStore = defineStore('order', () => {
+    const orders = ref<Order[]>([]);
+    const orderDetail = ref<OrderDetail>();
+
+    const isLoggedIn = ref(!!localStorage.getItem('token'));
+    console.log('init', isLoggedIn.value);
+
+    const userOrders = async () => {
+        try {
+            const tokenString = localStorage.getItem('token');
+
+            if (tokenString === null) {
+                // No hay token disponible, maneja esta situación adecuadamente
+                console.error('No token found in localStorage.');
+                return null; // Salimos de la función si no hay token
+            }
+
+            const tokenObj = JSON.parse(tokenString);
+
+            // fem una crida a la api
+            const response = await axios.get<Order[]>('/app/orders/', {
+                headers: {
+                    Authorization: `Bearer ${tokenObj.value}`
+                }
+            });
+            
+            if (response.status == 200) {
+                orders.value = response.data;
+            }
+        } catch (error) {
+            const errorMessage = error as ErrorResponse;
+            // mostrem els error en cas que no pugui retornar les dades
+            console.error('Error en obtenir les ordres', errorMessage.message);
+        }
+    };
+
+    const userOrderDetail = async (orderId: string | string []) => {
+        try {
+            const tokenString = localStorage.getItem('token');
+
+            if (tokenString === null) {
+                // No hay token disponible, maneja esta situación adecuadamente
+                console.error('No token found in localStorage.');
+                return null; // Salimos de la función si no hay token
+            }
+
+            const tokenObj = JSON.parse(tokenString);
+
+            // fem una crida a la api
+            const response = await axios.get<OrderDetail>(`/app/orders/details/detail/${orderId}`, {
+                headers: {
+                    Authorization: `Bearer ${tokenObj.value}`
+                }
+            });
+            
+            if (response.status == 200) {
+                orderDetail.value = response.data;
+            }
+        } catch (error) {
+            const errorMessage = error as ErrorResponse;
+            // mostrem els error en cas que no pugui retornar les dades
+            console.error("Error en obtenir l'order detail", errorMessage.message);
+        }
+    };
+
+    const deleteUserOrder = async (orderId: number) => {
+        try {
+            const tokenString = localStorage.getItem('token');
+
+            if (tokenString === null) {
+                // No hay token disponible, maneja esta situación adecuadamente
+                console.error('No token found in localStorage.');
+                return null; // Salimos de la función si no hay token
+            }
+
+            const tokenObj = JSON.parse(tokenString);
+
+            // fem crida a la api per eliminar l'order
+            const response = await axios.delete(`/app/orders/delete/${orderId}`, {
+                headers: {
+                    Authorization: `Bearer ${tokenObj.value}`
+                }
+            });
+    
+            if (response.status === 200) {
+                console.log('deleted')
+                router.push({ path: '/user/orders' });
+            }
+        } catch (error) {
+            const errorMessage = error as ErrorResponse;
+            // mostrem els error en cas que no pugui retornar les dades
+            console.error("Error en eliminar l'order", errorMessage.message);
+        }
+    };
+
+    return {userOrders, userOrderDetail, deleteUserOrder, orders, orderDetail}
+})
