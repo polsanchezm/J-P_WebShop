625010b1167f5e1b5ffd6ddfb8bfacac2ca079c8
Pol <polsamo2003@gmail.com>
2024-04-18 11:24:19 +0200

    Vistas creadas

---
+++ frontend/src/views/client/profile/shipping/AllShippingsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AllShippings from '@/components/client/profile/shipping/AllShippings.vue';
+</script>
+
+<template>
+    <div>
+        <AllShippings />
+    </div>
+</template>
---
+++ frontend/src/views/client/profile/shipping/ShippingEditView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ShippingEdit from '@/components/client/profile/shipping/ShippingEdit.vue';
+</script>
+
+<template>
+    <div>
+        <ShippingEdit />
+    </div>
+</template>
