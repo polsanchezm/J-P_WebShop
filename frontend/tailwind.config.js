208c8939405270857b515750c37d8885a0fb3d7a
Pol <polsamo2003@gmail.com>
2024-04-24 13:47:28 +0200

    Cambiado a en funcion del color del SO

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: 'class',
+    darkMode: 'media',
     content: ['./node_modules/flowbite/**/*.js'],
     theme: {
         extend: {
