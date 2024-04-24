684a28a135a0eba9c766a2038a4f4d26088532a6
Pol <polsamo2003@gmail.com>
2024-04-24 11:34:10 +0200

    Cambios visuales en formularios, header y footer

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -4,6 +4,8 @@ import image1 from '@/assets/fondo.png';
 
 import { ref } from 'vue';
 import { Carousel, Slide } from 'vue3-carousel';
+import { useAuthStore } from '@/stores/client/auth';
+const authStore = useAuthStore();
 
 const images = ref([
     { src: image1, alt: 'Descripción de la imagen 1' },
@@ -21,19 +23,19 @@ const images = ref([
 
 <template>
     <section class="header-background">
-        <div class="bg-gradient-to-br from-[#00000055] to-[#00000077]  h-full">
+        <div class="bg-gradient-to-b from-[#00000077] to-[#00000099] h-full">
             <div
                 class="container flex flex-col items-center justify-center mx-auto lg:p-36 p-4 text-center lg:min-h-screen min-h-[50vh] transition-all duration-1000">
                 <h1
-                    class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-[200px] font-lora dark:text-white">
-                    J&P</h1>
-                <p class="mb-8 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200">
-                    Discover
-                    your unique style with our versatile unisex collection at our store.</p>
+                    class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-neutral-100 md:text-5xl lg:text-[200px] font-lora dark:text-neutral-200">
+                    J &amp; P</h1>
+                <p
+                    class="mb-8 text-lg text-neutral-100 lg:text-xl sm:px-16 xl:px-48 dark:text-neutral-200 lg:text-[25px] font-bold">
+                    Discover your unique style with our versatile unisex collection at our store.</p>
                 <div
                     class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     <a href="#"
-                        class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700">
+                        class="explore-button inline-flex justify-center items-center py-4 px-6 text-base font-medium text-center rounded-lg dark:text-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-100 dark:hover:text-neutral-800 transition-colors">
                         Explore
                         <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
@@ -42,17 +44,17 @@ const images = ref([
                                 clip-rule="evenodd"></path>
                         </svg>
                     </a>
-                    <a href="#"
-                        class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800">
+                    <!-- <RouterLink v-if="authStore.isLoggedIn" :to="{ name: 'register' }" -->
+                    <RouterLink  :to="{ name: 'register' }"
+                        class="signup-button inline-flex justify-center items-center py-4 px-6 text-base font-medium text-center rounded-lg dark:text-neutral-800 dark:bg-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-100 transition-colors">
                         Sign Up
-                        <svg class="ml-2 -mr-1 w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        <svg class="ml-2 -mr-1 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 24 24">
                             <path fill-rule="evenodd"
                                 d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
-                                clip-rule="evenodd" />
+                                clip-rule="evenodd"></path>
                         </svg>
-                    </a>
+                    </RouterLink>
                 </div>
             </div>
         </div>
@@ -69,35 +71,8 @@ const images = ref([
 
 <style scoped>
 .header-background {
-    /* background-image: linear-gradient(to right, #581a94, #54178e, #501588, #4c1382, #48107c, #48107c, #48107c, #48107c, #4c1382, #501588, #54178e, #581a94); */
     background-image: url('@/assets/banner-background.webp');
     background-size: cover;
     background-position: center;
-}
-
-.signin-button {
-    background-color: #343a40;
-    color: #ffffff;
-    transition: background-color 0.3s ease, border-color 0.3s ease;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-    cursor: pointer;
-}
-
-.signin-button:hover {
-    background-color: #23272b;
-}
-
-.signup-button {
-    background-color: #ffffff;
-    color: #6c0ec2;
-    border: none;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-    transition: background-color 0.3s ease, color 0.3s ease;
-    cursor: pointer;
-}
-
-.signup-button:hover {
-    background-color: #6c0ec2;
-    color: #ffffff;
 }
 </style>
--- frontend/src/components/client/UserLogin.vue
+++ frontend/src/components/client/UserLogin.vue
@@ -20,25 +20,37 @@ const user = ref<User>({
 
 <template>
     <section>
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
-                    <form class="space-y-4 md:space-y-6" @submit.prevent="authStore.userLogin(user)">
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" v-model="user.email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="name@company.com" required />
-                            <!-- <span class="text-red-500">{{ errors.email }}</span> -->
+        <div class="flex flex-col items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
+            <div
+                class="w-full max-w-lg bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-6 md:space-y-6 sm:p-8">
+                    <h1
+                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Sign in to your account</h1>
+                    <form class="max-w-md mx-auto" @submit.prevent="authStore.userLogin(user)">
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="email" name="floating_email" id="floating_email" v-model="user.email"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_email"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
+                                address</label>
                         </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" v-model="user.currentPassword" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" required />
-                            <!-- <span class="text-red-500">{{ errors.password }}</span> -->
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="password" name="floating_password" id="floating_password" v-model="user.currentPassword"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_password"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                         </div>
-                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 utline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700">Sign in</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        <button type="submit"
+                            class="w-full text-white bg-gray-600 hover:bg-gray-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700">Sign
+                            in</button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400 mt-6">
                             Don't have an account yet?
-                            <RouterLink :to="{ name: 'register' }" class="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up </RouterLink>
+                            <RouterLink :to="{ name: 'register' }"
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up
+                            </RouterLink>
                         </p>
                     </form>
                 </div>
