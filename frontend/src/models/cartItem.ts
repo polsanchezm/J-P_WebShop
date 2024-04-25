94ff42dbda91fcd87657182d56f3ed3939237494
Pol <polsamo2003@gmail.com>
2024-04-25 20:03:55 +0200

    Eliminado

--- frontend/src/models/cartItem.ts
+++
@@ -1,9 +0,0 @@
-
-export interface CartItem {
-    id: number;
-    product_id: number;
-    size: string;
-    color: string;
-    stock: number | null;
-    quantity: number;
-}
--- frontend/src/models/wishlistItem.ts
+++
@@ -1,6 +0,0 @@
-export interface WishlistItem {
-    id: number;
-    wishlist_id: number,
-    variant_id: number,
-    quantity: number;
-}
