e46ad6b63ec533ea7a9ee5c4682de657d3b7a4fe
jeff-mcsc <129522162+jeff-mcsc@users.noreply.github.com>
2024-04-25 15:49:11 +0200

    Wishlist & Wishlist itemModels creats

---
+++ backend/app/Models/WishlistItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WishlistItem extends Model
+{
+    use HasFactory;
+    protected $table = 'wishlist_items';
+    protected $fillable = ['wishlist_id', 'variant_id', 'quantity'];
+
+    public function wishlist()
+    {
+        return $this->belongsTo(Wishlist::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(ProductVariant::class);
+    }
+}
