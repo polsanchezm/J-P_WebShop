2bc83df04f13fe051d3b391b937561aa0a43f38b
jeff-mcsc <129522162+jeff-mcsc@users.noreply.github.com>
2024-04-25 15:50:31 +0200

    Wishlist & wishlist items migrates creats

---
+++ backend/database/migrations/2024_04_23_132323_create_wishlist_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlist_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wishlist_id')->constrained('wishlists', 'id')->onDelete('cascade');
+            $table->foreignId('variant_id')->constrained('product_variants', 'id')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlist_items');
+    }
+};
