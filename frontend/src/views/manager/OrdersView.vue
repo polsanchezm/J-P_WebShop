cce7506c6a2392d8da63a511ed86d7d6bcd312fb
jeff-mcsc <129522162+jeff-mcsc@users.noreply.github.com>
2024-04-26 19:50:08 +0200

    Order view creat

---
+++ frontend/src/views/manager/OrdersView.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+import Orders from '@/components/manager/management/orders/Orders.vue';
+</script>
+
+<template>
+    <div>
+        <Orders />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
