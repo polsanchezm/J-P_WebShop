42f454091a5f097e70c5e41ca891ed8f9e0156d8
Pol <polsamo2003@gmail.com>
2024-04-29 09:47:47 +0200

    Formato dado

--- frontend/src/components/client/profile/UserOrderDetail.vue
+++ frontend/src/components/client/profile/UserOrderDetail.vue
@@ -23,16 +23,12 @@ onBeforeMount(async () => {
             <li v-for="(orderDetail, index) in orderStore.orderDetail" :key="index">
                 <p class="text-gray-700"><span class="font-semibold">ID:</span> {{ orderDetail!.id }}</p>
                 <p class="text-gray-700"><span class="font-semibold">Order ID:</span> {{ orderDetail!.order_id }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Variant ID:</span> {{ orderDetail!.variant_id }}
-                </p>
+                <p class="text-gray-700"><span class="font-semibold">Variant ID:</span> {{ orderDetail!.variant_id }}</p>
                 <p class="text-gray-700"><span class="font-semibold">Quantity:</span> {{ orderDetail!.quantity }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Purchase price:</span> {{
-                    orderDetail!.purchase_price }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Purchase price:</span> {{ orderDetail!.purchase_price }}</p>
             </li>
         </ul>
-        <button @click="orderStore.deleteUserOrder(orderId, true)"
-            class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none">Cancel
-            order</button>
+        <button @click="orderStore.deleteUserOrder(orderId, true)" class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none">Cancel order</button>
     </div>
 </template>
 
