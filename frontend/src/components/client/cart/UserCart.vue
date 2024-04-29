dd95e983798c78fa6539a4af0be747adbc46f551
Pol <polsamo2003@gmail.com>
2024-04-29 10:02:28 +0200

    Errores corregidos

--- frontend/src/components/client/cart/UserCart.vue
+++ frontend/src/components/client/cart/UserCart.vue
@@ -1,27 +1,34 @@
 <script setup lang="ts">
 import { useCartStore } from '@/stores/client/cart';
-import { onBeforeMount, computed } from 'vue';
+import { computed, onBeforeMount } from 'vue';
 
 const cartStore = useCartStore();
 
-onBeforeMount(async () => {
-    await cartStore.getCartFromCookie();
+onBeforeMount(() => {
+    cartStore.getCartFromCookie();
     console.log('cart', cartStore.cart);
+});
+
+const totalPrice = computed(() => {
+    const total = cartStore.cart.reduce((total, item: any) => {
+        return total + item.productPrice * item.quantity;
+    }, 0);
+    return total.toFixed(2);
 });
 </script>
 
 <template>
-    <div v-if="cartStore.cart" class="max-w-md mx-auto bg-white shadow-md rounded px-4 py-6">
+    <div v-if="cartStore.cart" class="max-w-md mx-auto bg-white shadow-md rounded px-4 py-6 mt-24">
         <p class="text-lg font-semibold mb-2 text-gray-700">Items</p>
         <button v-if="cartStore.cart.length > 0" @click="cartStore.removeAllFromCart()" class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none">Remove All</button>
         <ul>
             <li v-for="(item, index) in cartStore.cart" :key="index">
-                <p class="text-gray-700"><span class="font-semibold">ID:</span> {{ item!.id }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Name:</span> {{ item!.name }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Description:</span> {{ item!.description }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Image:</span> {{ item!.image }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Price:</span> {{ item!.price }}</p>
-                <p class="text-gray-700"><span class="font-semibold">Category ID:</span> {{ item!.category_id }}</p>
+                <p class="text-gray-700"><span class="font-semibold">ID:</span> {{ item!.productId }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Name:</span> {{ item!.productName }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Price:</span> {{ item!.productPrice }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Image:</span> {{ item!.productImage }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Variant ID:</span> {{ item!.variantId }}</p>
+                <p class="text-gray-700"><span class="font-semibold">Stock:</span> {{ item!.productStock }}</p>
                 <p class="text-gray-700"><span class="font-semibold">Quantity:</span> {{ item!.quantity }}</p>
 
                 <!-- <RouterLink
@@ -31,6 +38,7 @@ onBeforeMount(async () => {
                 <button @click="cartStore.incrementQuantity(item)" class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none">+</button>
                 <button @click="cartStore.removeFromCart(index)" class="inline-block mt-4 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 focus:outline-none">Remove</button>
             </li>
+            <p class="text-gray-700"><span class="font-semibold">Total price:</span> {{ totalPrice }}</p>
         </ul>
         <p v-if="cartStore.cart.length === 0">No items</p>
     </div>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,152 +8,157 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: () => import('@/components/MainHome.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('@/views/client/UserLoginView.vue')
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: () => import('@/views/client/UserRegisterView.vue')
-        },
-        {
-            path: '/logout',
-            name: 'logout',
-            component: () => import('@/components/client/UserLogout.vue')
-        },
-        {
-            path: '/user',
-            name: 'user',
+            component: () => import('@/components/LayoutComponent.vue'),
             children: [
                 {
-                    path: '',
-                    name: 'user.detail',
-                    component: () => import('@/views/client/profile/UserDetailView.vue'),
-                    meta: { requiresAuth: true }
+                    path: '/login',
+                    name: 'login',
+                    component: () => import('@/views/client/UserLoginView.vue')
                 },
                 {
-                    path: 'edit',
-                    name: 'user.edit',
-                    component: () => import('@/views/client/profile/UserEditView.vue'),
-                    meta: { requiresAuth: true }
+                    path: '/login',
+                    name: 'login',
+                    component: () => import('@/views/client/UserLoginView.vue')
                 },
                 {
-                    path: 'orders',
+                    path: '/register',
+                    name: 'register',
+                    component: () => import('@/views/client/UserRegisterView.vue')
+                },
+                {
+                    path: '/logout',
+                    name: 'logout',
+                    component: () => import('@/components/client/UserLogout.vue')
+                },
+                {
+                    path: '/user',
+                    name: 'user',
                     children: [
                         {
                             path: '',
-                            name: 'orders',
-                            component: () => import('@/views/client/profile/UserOrdersView.vue')
+                            name: 'user.detail',
+                            component: () => import('@/views/client/profile/UserDetailView.vue'),
+                            meta: { requiresAuth: true }
+                        },
+                        {
+                            path: 'edit',
+                            name: 'user.edit',
+                            component: () => import('@/views/client/profile/UserEditView.vue'),
+                            meta: { requiresAuth: true }
+                        },
+                        {
+                            path: 'orders',
+                            children: [
+                                {
+                                    path: '',
+                                    name: 'orders',
+                                    component: () => import('@/views/client/profile/UserOrdersView.vue')
+                                },
+                                {
+                                    path: 'detail/:id',
+                                    name: 'orderDetail',
+                                    component: () => import('@/views/client/profile/UserOrderDetailView.vue')
+                                }
+                            ]
+                        },
+                        {
+                            path: 'shipping',
+                            name: 'shipping',
+                            children: [
+                                {
+                                    path: '',
+                                    name: 'shipping.all',
+                                    component: () => import('@/views/client/profile/shipping/AllShippingsView.vue'),
+                                    meta: { requiresAuth: true }
+                                },
+                                {
+                                    path: 'create',
+                                    name: 'shipping.create',
+                                    component: () => import('@/views/client/profile/shipping/ShippingCreateView.vue'),
+                                    meta: { requiresAuth: true }
+                                },
+                                {
+                                    path: 'edit/:id',
+                                    name: 'shipping.edit',
+                                    component: () => import('@/views/client/profile/shipping/ShippingEditView.vue'),
+                                    meta: { requiresAuth: true }
+                                }
+                            ]
+                        },
+                        {
+                            path: 'cart',
+                            name: 'cart',
+                            component: () => import('@/views/client/cart/CartView.vue')
+                        },
+                        {
+                            path: 'wishlist',
+                            name: 'wishlist',
+                            component: () => import('@/views/client/wishlist/WishlistView.vue')
+                        }
+                    ]
+                },
+                {
+                    path: '/products',
+                    children: [
+                        {
+                            path: '',
+                            name: 'products',
+                            component: () => import('@/views/product/ProductsView.vue')
                         },
                         {
                             path: 'detail/:id',
-                            name: 'orderDetail',
-                            component: () => import('@/views/client/profile/UserOrderDetailView.vue')
+                            name: 'productDetail',
+                            component: () => import('@/views/product/ProductDetailView.vue')
                         }
                     ]
                 },
                 {
-                    path: 'shipping',
-                    name: 'shipping',
+                    path: '/management',
+                    name: 'management',
                     children: [
                         {
-                            path: '',
-                            name: 'shipping.all',
-                            component: () => import('@/views/client/profile/shipping/AllShippingsView.vue'),
-                            meta: { requiresAuth: true }
+                            path: 'products',
+                            children: [
+                                {
+                                    path: '',
+                                    name: 'products.manager',
+                                    component: () => import('@/views/manager/ProductsView.vue'),
+                                    meta: { requiresAuth: true }
+                                },
+                                {
+                                    path: 'add',
+                                    name: 'product.add',
+                                    component: () => import('@/views/manager/AddProductView.vue'),
+                                    meta: { requiresAuth: true }
+                                },
+                                {
+                                    path: 'detail/:id',
+                                    name: 'product.detail.manager',
+                                    component: () => import('@/views/manager/ProductDetailView.vue'),
+                                    meta: { requiresAuth: true }
+                                },
+                                {
+                                    path: 'edit/:id',
+                                    name: 'product.edit',
+                                    component: () => import('@/views/manager/EditProductView.vue'),
+                                    meta: { requiresAuth: true }
+                                }
+                            ]
                         },
                         {
-                            path: 'create',
-                            name: 'shipping.create',
-                            component: () => import('@/views/client/profile/shipping/ShippingCreateView.vue'),
-                            meta: { requiresAuth: true }
-                        },
-                        {
-                            path: 'edit/:id',
-                            name: 'shipping.edit',
-                            component: () => import('@/views/client/profile/shipping/ShippingEditView.vue'),
+                            path: 'orders',
+                            name: 'orders.manager',
+                            component: () => import('@/views/manager/OrdersView.vue'),
                             meta: { requiresAuth: true }
                         }
                     ]
                 },
                 {
-                    path: 'cart',
-                    name: 'cart',
-                    component: () => import('@/views/client/cart/CartView.vue'),
-                    meta: { requiresAuth: true }
-                },
-                {
-                    path: 'wishlist',
-                    name: 'wishlist',
-                    component: () => import('@/views/client/wishlist/WishlistView.vue'),
-                    meta: { requiresAuth: true }
+                    path: '/:pathMatch(.*)*',
+                    name: 'error404',
+                    component: () => import('@/views/Error404View.vue')
                 }
             ]
-        },
-        {
-            path: '/products',
-            children: [
-                {
-                    path: '',
-                    name: 'products',
-                    component: () => import('@/views/product/ProductsView.vue')
-                },
-                {
-                    path: 'detail/:id',
-                    name: 'productDetail',
-                    component: () => import('@/views/product/ProductDetailView.vue')
-                }
-            ]
-        },
-        {
-            path: '/management',
-            name: 'management',
-            children: [
-                {
-                    path: 'products',
-                    children: [
-                        {
-                            path: '',
-                            name: 'products.manager',
-                            component: () => import('@/views/manager/ProductsView.vue'),
-                            meta: { requiresAuth: true }
-                        },
-                        {
-                            path: 'add',
-                            name: 'product.add',
-                            component: () => import('@/views/manager/AddProductView.vue'),
-                            meta: { requiresAuth: true }
-                        },
-                        {
-                            path: 'detail/:id',
-                            name: 'product.detail.manager',
-                            component: () => import('@/views/manager/ProductDetailView.vue'),
-                            meta: { requiresAuth: true }
-                        },
-                        {
-                            path: 'edit/:id',
-                            name: 'product.edit',
-                            component: () => import('@/views/manager/EditProductView.vue'),
-                            meta: { requiresAuth: true }
-                        }
-                    ]
-                },
-                {
-                    path: 'orders',
-                    name: 'orders.manager',
-                    component: () => import('@/views/manager/OrdersView.vue'),
-                    meta: { requiresAuth: true }
-                }
-            ]
-        },
-        {
-            path: '/:pathMatch(.*)*',
-            name: 'error404',
-            component: () => import('@/views/Error404View.vue')
         }
     ]
 });
