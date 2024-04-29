ee07a0693f59a76f3b3e2b29458602069af40f23
Pol <polsamo2003@gmail.com>
2024-04-29 11:19:42 +0200

    Comentarios eliminados

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // CategorySeeder::class,
-            // ProductSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
