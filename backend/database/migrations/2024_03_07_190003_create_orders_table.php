907b4b61bae50b83043f495b9c0c244a7508e397
Pol <polsamo2003@gmail.com>
2024-05-06 18:56:30 +0200

    Correcion del backend

--- backend/database/migrations/2024_03_07_190003_create_orders_table.php
+++ backend/database/migrations/2024_03_07_190003_create_orders_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->date('order_date');
             $table->timestamps();
         });
     }
